dc882d7023c452e2b634e97fb735e5d4ebbc508c
Matt Bhagat-Conway <matt@indicatrix.org>
2025-12-17 10:35:31 -0500

    make rounding in fuzzing consistent with rounding in identificatiion

--- src/fuzz.jl
+++ src/fuzz.jl
@@ -31,10 +31,14 @@ function fuzz(G::FuzzedGraph, mlg, links)
                 # these potentially should have a link
                 # check net distance from closest points
                 pt_e1, pt_e2 = LibGEOS.nearestPoints(e1.geometry, e2.geometry)
-                len_e1 = AG.geomlength(e1.geometry)
-                pos_e1 = LibGEOS.project(e1.geometry, pt_e1)
-                len_e2 = AG.geomlength(e2.geometry)
-                pos_e2 = LibGEOS.project(e2.geometry, pt_e2)
+
+                # round everything - this accumulates a small amount of rounding error,
+                # which we deem acceptable, and this way rounding error accumulates the same
+                # way in fuzzing as in the main code.
+                len_e1 = round(UInt16, AG.geomlength(e1.geometry))
+                pos_e1 = round(UInt16, LibGEOS.project(e1.geometry, pt_e1))
+                len_e2 = round(UInt16, AG.geomlength(e2.geometry))
+                pos_e2 = round(UInt16, LibGEOS.project(e2.geometry, pt_e2))
 
                 net_dist = min(
                     pos_e1 + dmat[e1.src, e2.src] + pos_e2,
@@ -43,7 +47,9 @@ function fuzz(G::FuzzedGraph, mlg, links)
                     (len_e1 - pos_e1) + dmat[e1.dst, e2.dst] + (len_e2 - pos_e2)
                 )
 
-                if net_dist > MIN_NET_DIST
+                # if the net dist is actually say 1000.25m, we want to consider not greater than 1000m,
+                # because distances all get rounded inside the missinglinks tool.
+                if round(UInt16, min(net_dist, typemax(UInt16))) > MIN_NET_DIST
                     found_this_link = false
 
                     # These should have a link (unless they were part of an island - todo)
@@ -75,7 +81,7 @@ function fuzz(G::FuzzedGraph, mlg, links)
                     end
 
                     if !found_this_link
-                        @warn "Link from $pt_e1 @ $pos_e1 / $len_e1 to $pt_e2 @ $pos_e2 / $len_e2 with length $geog_dist not found, seed $(G.settings.seed)"
+                        @warn "Link from $pt_e1 @ $pos_e1 / $len_e1 to $pt_e2 @ $pos_e2 / $len_e2 with length $geog_dist (network $net_dist) not found, seed $(G.settings.seed)"
                     end
                 end
             end
@@ -90,11 +96,13 @@ function fuzz(G::FuzzedGraph, mlg, links)
 end
 
 function fuzzer(n; settings=FuzzedGraphSettings(), seed=nothing)
+    # if seed is specified, use it to seed the RNG, but also use it as the seed for the first fuzz so things can be reproduced
     rng = StableRNG(isnothing(seed) ? rand(UInt64) : seed)
+    seed = isnothing(seed) ? rand(rng, UInt64) : seed
     f = nothing
     G = nothing
     @showprogress for _ in 1:n
-        settings.seed = rand(rng, UInt64)
+        settings.seed = seed 
         fuzzed = build_fuzzed_graph(settings)
 
         all_links = with_logger(NullLogger()) do
@@ -108,6 +116,9 @@ function fuzzer(n; settings=FuzzedGraphSettings(), seed=nothing)
         end
 
         f = fuzz(fuzzed, G, all_links)
+
+        # reset seed for next iteration
+        seed = rand(rng, UInt64)
     end
 
     return (links=f, G=G)
